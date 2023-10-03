df81a27e711a12d600681614fb6324ea61315575
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-10-04 00:21:10 +0600

    Update HeroiconDefaultFormatter.php

--- src/Plugin/Field/FieldFormatter/HeroiconDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/HeroiconDefaultFormatter.php
@@ -18,7 +18,20 @@ use Drupal\Core\Field\FormatterBase;
  */
 class HeroiconDefaultFormatter extends FormatterBase {
 
-  public function viewElements(FieldItemListInterface $items, $langcode) {
-    // Your view elements code here.
-  }
+    public function viewElements(FieldItemListInterface $items, $langcode) {
+        $elements = [];
+
+        foreach ($items as $delta => $item) {
+          $icon_name = $item->icon_name;
+          $icon_style = $item->icon_style;
+
+          // You can customize this part to render the icon as you like.
+          $elements[$delta] = [
+            '#markup' => '<span class="heroicon-' . $icon_style . '">' . $icon_name . '</span>',
+          ];
+        }
+
+        return $elements;
+      }
+
 }
